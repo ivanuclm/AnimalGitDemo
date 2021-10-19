bd25a326f5db31a5b34e6288afebc08c65b51765
ivfct <i.mora@campus.fct.unl.pt>
2021-10-19 22:18:21 +0100

    comele los super huevos a mi perro y a mi gato que se llaman jesus y damian

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 /**
- * @author Miguel Goulao, n. xxxxx
+ * @author Ivan Hernandez, nº 62448
  * Demo for the Git lab on Software Engineering
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Super Animal git demo");
+        System.out.println("Let's hear it from the pets");
+        Animal dog = new Dog("Jesus");
+        Animal cat = new Cat("Damian");
+        System.out.println(dog.getName() + " says " + dog.speak());
+        System.out.println(cat.getName() + " says " + cat.speak());
     }
 }
